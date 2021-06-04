f138d6e40bce61d5edae65d2ff46ada280a7ebc5
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-04 19:07:33 +0300

    feat: add userRouter

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./resources/users/user.router');
 
 app.use(express.json());
+
+app.use('/api', (req, res, next) => {
+    if (req.originalUrl === '/api/') {
+        res.send('Service is running!');
+        return;
+    }
+    next();
+});
+
+app.use('/api/users', userRouter);
 
 export = app;
