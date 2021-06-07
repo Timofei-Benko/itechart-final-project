0f8bbb98af8354f103a07f4a3be77a6f689e545a
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-07 18:20:44 +0300

    feat: add morgan-body

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,17 @@
 const express = require('express');
+const morganBody = require('morgan-body');
 const app = express();
 const userRouter = require('./resources/users/user.router');
+const createDBConnection = require('./db');
 
 app.use(express.json());
 
+morganBody(app);
+
+createDBConnection();
+
 app.use('/api', (req, res, next) => {
-    if (req.originalUrl === '/api/') {
+    if (req.originalUrl === '/api') {
         res.send('Service is running!');
         return;
     }
