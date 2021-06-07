2962220f1053fbb2e9c05f9e7e6d284303452d7c
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-07 18:24:15 +0300

    feat: add user sign in and delete routes

--- server/src/resources/users/user.router.ts
+++ server/src/resources/users/user.router.ts
@@ -1,21 +1,82 @@
 const router = require('express').Router();
-const User = require('./user.model');
+const bcrypt = require('bcrypt');
 
-router.route('/create').post(async (req, res) => {
-    const { firstName, lastName, email, password } = req.body.user;
-    console.log(req.body)
-    const user = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-    });
+import IUser = require('./user.interface');
+const userService = require('./user.service');
 
+router.route('/signup').post(async (req, res, next) => {
     try {
-        await user.save();
-        res.status(201).json(req.body.user);
-    } catch (err) {
-        res.status(500).json( { error: err.message } );
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            username,
+            position,
+            experience,
+            languages
+        }: IUser = req.body.user;
+
+        if (await userService.exists({ email })) {
+            return res.status(409).json({error: 'User already exists'});
+        }
+
+        const userData = {
+            firstName,
+            lastName,
+            email,
+            passwordHash: bcrypt.hashSync(password, 10),
+            username,
+            position,
+            experience,
+            languages
+        };
+
+        await userService.create(userData);
+
+        res.status(201).json(
+            {
+                status: 'User created',
+                userData: userService.getSafeResponse(req.body.user),
+            }
+        );
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/signin').post(async (req, res, next) => {
+    try {
+        const { email, password }: { email: string, password: string } = req.body.user;
+
+        if (!await userService.exists({ email })) {
+            return res.status(404).json({error: 'User not found'});
+        }
+
+        const user = await userService.getOne(email);
+
+        console.log(user, password)
+
+        // bcrypt.compare(password)
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/:userId').delete(async (req, res, next) => {
+    try {
+        const userId: string = req.params.userId;
+        console.log(userId);
+
+        if (await userService.exists(userId)) {
+            await userService.deleteOneById(userId);
+            return res.status(200).json({ status: 'User deleted' })
+        } else {
+            return res.status(404).json({ error: 'User doesn\'t exist' });
+        }
+    } catch (e) {
+        next(e);
     }
 });
 
