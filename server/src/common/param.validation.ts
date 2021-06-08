6ff4f0da8aaf216de29f838cf3021706ea83448f
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-08 16:18:33 +0300

    delete: move methods to user.service.ts

--- server/src/common/param.validation.ts
+++
@@ -1,6 +0,0 @@
-const isValidPassword = (password: string): boolean => password.length > 4;
-
-const isValidEmail = (email: string): boolean => {
-    const REG_EXP: RegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return REG_EXP.test(email);
-};
