accfe2a432bfc3ed23414606bd144892b9249f95
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-11 18:14:24 +0300

    feat: add new types for new props

--- server/src/resources/users/user.interface.ts
+++ server/src/resources/users/user.interface.ts
@@ -8,6 +8,10 @@ interface IUser {
     position?: string;
     experience?: number;
     languages?: Array<string>;
+    questionQty: number;
+    answerQty: number;
+    likedAnswerQty: number;
+    bestAnswerQty: number;
 }
 
 export = IUser;
