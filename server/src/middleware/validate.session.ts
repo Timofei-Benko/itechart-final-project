fbc9a9563db6d438677b6f579d10128b5818e54b
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-15 17:13:29 +0300

    fix: add proper value to put into request if user validation is successful

--- server/src/middleware/validate.session.ts
+++ server/src/middleware/validate.session.ts
@@ -7,9 +7,9 @@ const validateSession = (req: e.Request, res: e.Response, next: e.NextFunction) 
     try {
         // @ts-ignore
         const token: string | undefined = req.headers.authorization.split(' ')[1].toString();
-        jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+        jwt.verify(token, JWT_SECRET_KEY, async (err, decoded) => {
             if (decoded) {
-                req.body.user = userService.getOne({_id: decoded.id});
+                req.body.user = await userService.getSafeResponse(await userService.getOne({ _id: decoded.id }));
                 next();
             }
             if (err) return res.status(401).json({ error: `Not authorized: ${err}` });
