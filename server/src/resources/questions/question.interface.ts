763592f500ff18a85265e5624011da81c58ae376
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-17 17:52:13 +0300

    refactor: update interface

--- server/src/resources/questions/question.interface.ts
+++ server/src/resources/questions/question.interface.ts
@@ -1,8 +1,9 @@
 import IAnswer = require('./answer.interface');
+import IUser = require('../users/user.interface');
 
 interface IQuestion {
     _id?: string;
-    user: string;
+    user: string | IUser;
     title: string;
     content: string;
     answers?: Array<IAnswer>;
