00e4b59791630def42ad114c1fb28a32f614bbc6
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-17 21:08:21 +0300

    feat: set up client environment

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from "react";
+
+const App: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    );
+};
+
+export default App;
