3d6b203080b31f6bcbe60c7b2e39efd3268dcda2
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-17 17:55:55 +0300

    refactor: remove regexp type assignment

--- server/src/resources/users/user.service.ts
+++ server/src/resources/users/user.service.ts
@@ -36,7 +36,7 @@ const getSafeResponse = (userData: IUser): Omit<IUser, 'password' | 'passwordHas
 const isValidPassword = (password: string): boolean => password.length > 4;
 
 const isValidEmail = (email: string): boolean => {
-    const REG_EXP: RegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const REG_EXP = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return REG_EXP.test(email);
 };
 
@@ -57,9 +57,9 @@ const create = async (userData: IUser): Promise<IUser> => {
     return user;
 }
 
-const getOne = async (filter: object): Promise<IUser & { _id: string }> => User.findOne(filter).exec();
+const getOne = async (filter: object): Promise<IUser & { _id: string }> => User.findOne(filter);
 
-const updateStats = async (userId: string, statsItem: string): Promise<void> => {
+const updateStats = async (userId: string | IUser, statsItem: string): Promise<void> => {
     const user = await User.findOne({ _id: userId });
     await User.findOneAndUpdate({ _id: userId }, {
         [statsItem]: user[statsItem] + 1,
