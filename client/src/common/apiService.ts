0b19aea066282855ab1ece3216e480169a1aadd7
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-22 20:42:22 +0300

    erafcator: change env var name

--- client/src/common/apiService.ts
+++ client/src/common/apiService.ts
@@ -1,7 +1,8 @@
+import React from "react";
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: `http://localhost:5000/api`,
+    baseURL: process.env.API_HOST,
 });
 
 const signUp = async (userData) => {
