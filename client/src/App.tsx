aaaa8f44ca0ba8e8c955ce06c7f38a69c6cd609d
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-22 20:12:59 +0300

    feat: add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
 import React, { FunctionComponent } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
 import 'normalize.css';
 import GlobalStyle from "./components/dumb/GlobalStyle";
-import SignUp from './pages/SignUp';
+
 
 const App: FunctionComponent = (): JSX.Element => {
     return (
-        <>
+        <Router>
             <GlobalStyle/>
-            <SignUp/>
-        </>
+            <Switch>
+                <Route path='/login'>
+                    <SignIn />
+                </Route>
+                <Route path='/register'>
+                    <SignUp />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
