c0415c34887371d4a895632ee8c15430a9f2708e
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-01 17:05:30 +0300

    refactor: remove unused import

--- client/src/pages/PersonalSpace.tsx
+++ client/src/pages/PersonalSpace.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useRef, useEffect } from 'react';
+import React, { FunctionComponent, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components/macro';
