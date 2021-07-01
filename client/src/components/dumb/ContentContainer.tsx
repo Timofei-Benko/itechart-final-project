6d59674375eedeae92342bc670865c8ed4be676d
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-01 15:50:45 +0300

    feat: add prop to manage component styles

--- client/src/components/dumb/ContentContainer.tsx
+++ client/src/components/dumb/ContentContainer.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const ContentContainer = styled.div`
   display: flex;
-  min-height: 100vh;
+  min-height: ${ (props: { outer?: boolean, inner?: boolean }) => (props.outer && '100vh') || (props.inner && '100%') };
+  width: 100%;
   max-width: 1200px;
   margin: 0 auto;
   align-items: center;
