54b4d000536362dc40af54a00dda7e8d1dabc4fb
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:33:24 +0300

    feat: add userQuestions state hook

---
+++ client/src/redux/hooks/useUserQuestionStore.ts
@@ -0,0 +1,31 @@
+import * as apiService from '../../common/apiService';
+import {
+    SET_USER_QUESTION_DATA_LOADING,
+    SET_USER_QUESTION_DATA_SUCCESS,
+    SET_USER_QUESTION_DATA_ERROR,
+} from "../actions";
+
+export default function useUserQuestionStore() {
+    return async dispatch => {
+        dispatch({
+            type: SET_USER_QUESTION_DATA_LOADING,
+        });
+
+        const userId = localStorage.getItem('user_id') as string;
+
+        try {
+            const response = await apiService.getAllUserQuestions(userId, '?order=desc');
+
+            dispatch({
+                type: SET_USER_QUESTION_DATA_SUCCESS,
+                payload: response.data.questions,
+            });
+
+        } catch (e) {
+            dispatch({
+                type: SET_USER_QUESTION_DATA_ERROR,
+                payload: e,
+            });
+        }
+    };
+};
