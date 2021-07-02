6df3b3fa76f9075fdd29c6b98871477adc97434f
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:29:08 +0300

    refactor: rename method

--- client/src/redux/hooks/useUserStore.ts
+++ client/src/redux/hooks/useUserStore.ts
@@ -1,7 +1,7 @@
 import * as apiService from '../../common/apiService';
 import { SET_USER_DATA_LOADING, SET_USER_DATA_SUCCESS, SET_USER_DATA_ERROR } from "../actions";
 
-export default function getUserData() {
+export default function useUserStore() {
     return async dispatch => {
         dispatch({
             type: SET_USER_DATA_LOADING,
