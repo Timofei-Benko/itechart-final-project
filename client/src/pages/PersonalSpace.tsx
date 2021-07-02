0d790291b49a4b4ec7a03f09689d1747f0e077d2
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:34:27 +0300

    refactor: separate into components

--- client/src/pages/PersonalSpace.tsx
+++ client/src/pages/PersonalSpace.tsx
@@ -1,42 +1,35 @@
 import React, { FunctionComponent, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useRouteMatch, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
-import isSignedIn from '../common/authentication/isSignedIn';
 import { RootState } from '../common/config/interfaces';
 
 import Header from '../components/smart/Header';
+import Sidebar from '../components/smart/Sidebar';
+import UserInformation from '../components/smart/UserInformation';
+import Questions from '../components/smart/UserQuestions';
 
 import ContentContainer from '../components/dumb/ContentContainer';
-import Delimiter from '../components/dumb/Delimiter';
+import Spinner from '../components/dumb/Spinner';
+
+import { HEADER_HEIGHT, BOX_SHADOW } from '../components/constants';
 
 const PersonalSpaceContainer = styled.div`
-  padding: 2rem;
-  width: 100%;
-  height: 100%;
-  background-color: #FFFFFF;
-`;
-
-const Title = styled.h2`
-  text-transform: capitalize
-`;
-
-const TextContainer = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 1rem;
+  margin: 2rem;
+  padding: 2rem;
+  min-height: 650px;
+  width: 100%;
+  border-radius: 15px;
+  height: calc(100% - ${HEADER_HEIGHT});
+  background-color: #FFFFFF;
+  box-shadow: ${BOX_SHADOW};
+  transition: all .2s ease-in-out;
 `;
 
-const TextItem = styled.span`
-  display: block;
-  max-width: 500px;
-  
-  span {
-    font-weight: bold;
-  }
-`;
+const PSContentContainer = styled.div``;
 
 const PersonalSpace: FunctionComponent = (): JSX.Element => {
 
@@ -44,98 +37,38 @@ const PersonalSpace: FunctionComponent = (): JSX.Element => {
     const userState = useRef(initUserState);
     userState.current = initUserState;
 
+    const { path } = useRouteMatch();
+
     return (
-        !isSignedIn() ?
-            <Redirect to={'/login'} />
-            :
-            <>
-                <Header />
-                <ContentContainer inner={true}>
+        <>
+            <Header />
+            <ContentContainer height={`calc(100% - ${HEADER_HEIGHT})`}>
                     <PersonalSpaceContainer>
-                        <Title>Account information</Title>
-                        <TextContainer>
-                            <TextItem>
-                                <span>Full Name: </span>
-                                { `${userState.current.data.firstName} ${userState.current.data.lastName}` }</TextItem>
-                            <TextItem>
-                                <span>Email: </span>
-                                { userState.current.data.email }
-                            </TextItem>
-                        </TextContainer>
-
-                        <Delimiter />
-
-                        <Title>User Information</Title>
-                        <TextContainer>
-                            {
-                                userState.current.data.username
-                                &&
-                                <TextItem>
-                                    <span>Username: </span>
-                                    { userState.current.data.username }
-                                </TextItem>
-                            }
-
-                            {
-                                userState.current.data.experience
-                                &&
-                                <TextItem>
-                                    <span>Experience: </span>
-                                    {
-                                        `
-                                            ${userState.current.data.experience} 
-                                            ${userState.current.data.experience === 1 ? 'year' : 'years'}
-                                        `
-                                    }
-                                </TextItem>
-                            }
-
-                            {
-                                userState.current.data.position
-                                &&
-                                <TextItem>
-                                    <span>Position: </span>
-                                    { userState.current.data.position }
-                                </TextItem>
-                            }
-
-                            {
-                                userState.current.data.languages
-                                    &&
-                                userState.current.data.languages.length !== 0
-                                    &&
-                                <TextItem>
-                                    <span>Preferred languages: </span>
-                                    { userState.current.data.languages.join(', ') }
-                                </TextItem>
-                            }
-
-                        </TextContainer>
-
-                        <Delimiter />
-
-                        <Title>User Statistics</Title>
-                        <TextContainer>
-                            <TextItem>
-                                <span>Questions posted: </span>
-                                { userState.current.data.questionQty }
-                            </TextItem>
-                            <TextItem>
-                                <span>Answers posted: </span>
-                                { userState.current.data.answerQty }
-                            </TextItem>
-                            <TextItem>
-                                <span>Best answer quantity: </span>
-                                { userState.current.data.bestAnswerQty }
-                            </TextItem>
-                            <TextItem>
-                                <span>Popular answer quantity: </span>
-                                { userState.current.data.likedAnswerQty }
-                            </TextItem>
-                        </TextContainer>
+                        {
+                            userState.current.loading
+                                ?
+                                <Spinner />
+                                :
+                                <>
+                                    <Sidebar path={path}/>
+                                    <PSContentContainer>
+                                        <Switch>
+                                            <Route exact path={`${path}/`}>
+                                                <Redirect to={`${path}/account`} />
+                                            </Route>
+                                            <Route exact path={`${path}/account`}>
+                                                <UserInformation />
+                                            </Route>
+                                            <Route exact path={`${path}/questions`}>
+                                                <Questions />
+                                            </Route>
+                                        </Switch>
+                                    </PSContentContainer>
+                                </>
+                        }
                     </PersonalSpaceContainer>
-                </ContentContainer>
-            </>
+            </ContentContainer>
+        </>
     )
 };
 
