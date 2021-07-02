85daed9dca1b067ec6c5044c0a84bb49ca855591
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:35:25 +0300

    refactor: import contant to use as box-shadow value

--- client/src/components/dumb/Form.tsx
+++ client/src/components/dumb/Form.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro';
+import { BOX_SHADOW } from '../constants';
 
 export default styled.form`
   display: flex;
@@ -9,10 +10,5 @@ export default styled.form`
   background: white ;
   padding: 0 3rem 1rem 3rem;
   border-radius: 4px;
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-  0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-  0 12.5px 10px rgba(0, 0, 0, 0.06),
-  0 22.3px 17.9px rgba(0, 0, 0, 0.072),
-  0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-  0 100px 80px rgba(0, 0, 0, 0.12);
+  box-shadow: ${BOX_SHADOW};
 `;
