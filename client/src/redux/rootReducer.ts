5f36acbe77f1c21c9fd43d9b60e381a6ed00a130
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:31:23 +0300

    feat: add reducer userQuestions state

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -3,12 +3,14 @@ import { combineReducers } from "redux";
 import signUpErrorReducer from './reducers/signUpErrorReducer';
 import authenticationStatusReducer from "./reducers/authenticationStatusReducer";
 import userReducer from "./reducers/userReducer";
+import userQuestionsReducer from './reducers/userQuestionsReducer';
 
 const rootReducer = combineReducers(
     {
         signUpError: signUpErrorReducer,
         authenticationStatus: authenticationStatusReducer,
         userState: userReducer,
+        userQuestionState: userQuestionsReducer,
     }
 );
 
