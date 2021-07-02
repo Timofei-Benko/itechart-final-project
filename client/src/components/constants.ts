42f4f1caf1817d694811237df16fb7863791cb94
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-02 18:32:24 +0300

    feat: add styled-comp constants

--- client/src/components/constants.ts
+++ client/src/components/constants.ts
@@ -1,5 +1,14 @@
-const INPUT_HEIGHT = '40px';
+const INPUT_HEIGHT: string = '40px';
+const HEADER_HEIGHT: string = '75px';
+const BOX_SHADOW: string = `0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+    0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+    0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+    0 100px 80px rgba(0, 0, 0, 0.12)`
 
 export {
     INPUT_HEIGHT,
+    HEADER_HEIGHT,
+    BOX_SHADOW,
 }
